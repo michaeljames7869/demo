8de8731da8f27c79bc14dceb05e79aecdf48c97d
michaeljames7869 <58004358+michaeljames7869@users.noreply.github.com>
2019-11-29 00:34:51 +0530

    Update Gruntfile.js

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function (grunt) {
-	"use strict";
+	"use strict"; 
 	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
 	grunt.config.merge({ compatVersion: "edge" });
 	grunt.registerTask("default", ["lint"]);
